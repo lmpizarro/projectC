81061ac0c4de4cea87393c572ccf35890b63213b
lmpizarro <lmpizarro@gmail.com>
2022-11-09 00:08:04 -0300

    added weekends

--- src/bonos/yield_curve.py
+++ src/bonos/yield_curve.py
@@ -23,19 +23,42 @@ def download():
         t = Thread(target=task, args=(year,)).start()
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-columns = ['1 Mo', '2 Mo', '3 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr', '7 Yr', '10 Yr', '20 Yr', '30 Yr']
+"https://home.treasury.gov/policy-issues/financing-the-government/interest-rate-statistics"
+columns = ['1 Mo', '2 Mo', '4 Mo', '3 Mo', '6 Mo', '1 Yr', '2 Yr', '3 Yr', '5 Yr', '7 Yr', '10 Yr', '20 Yr', '30 Yr']
+columns_m = [1, 2, 3, 4, 6, 12, 24, 36, 60, 72, 120, 240, 360]
+map_y = {c: columns_m[i] for i, c in enumerate(columns)}
+years = {}
 if __name__ == "__main__":
     from pathlib import Path
     curve_path = Path('yield_curve').glob('*.pkl')
     for c in curve_path:
+        year = c.stem.split('_')[1]
         df = pd.read_pickle(c)
         if '30 Yr' not in df.keys():
             df['30 Yr'] = df['20 Yr']
         if '2 Mo' not in df.keys():
-            df['2 Mo'] = df['1 Mo'] + df['3 Mo']
+            df['2 Mo'] = ((df['1 Mo'] + df['3 Mo']) / 2).round(2)
+        if '4 Mo' not in df.keys():
+            df['4 Mo'] = ((2*df['3 Mo'] + df['6 Mo']) / 3).round(2)
 
-        if df['2 Mo'].isnull().sum():
-            print('dddd')
+        df.rename(map_y, axis=1, inplace=True)
+        df[columns_m] = df[columns_m].interpolate(method='linear', axis=1)
+        # df.set_index('Date', inplace=True)
+        df['Date'] = pd.to_datetime(df['Date'])
+        df.set_index('Date', inplace=True)
+        df = df.reindex(pd.date_range(df.index.min(), df.index.max())).sort_index(ascending=False).reset_index().rename(columns={'index': 'Date'})
+        df[columns_m] = df[columns_m].interpolate(method='linear', axis=0)
+        df = df[columns_m]
+        years[year] = df
 
-        print(df[columns].tail())
+        print(df.head(10))
+
+    print(years.keys())
+
+    begin_y = 2008
+    end_y   = 2018
+
+    for year in range(begin_y, end_y + 1):
+        print(year)
