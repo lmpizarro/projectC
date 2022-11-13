5f9c25ad8fc7c040dcf3b452cab9f7548dbbc35a
lmpizarro <lmpizarro@gmail.com>
2022-11-13 12:05:58 -0300

    class for read csvs

--- src/bonos/diarios/main.py
+++ src/bonos/diarios/main.py
@@ -10,7 +10,6 @@ DAYS_IN_A_YEAR = 364
 
 import scipy.optimize
 
-
 class Fit:
     @staticmethod
     def monoExp(x, m, t, b):
@@ -27,82 +26,82 @@ class Fit:
     def polyModel(rs, vs):
         return np.poly1d(np.polyfit(rs, vs, 8))
 
+class LeerCSVS:
+    @staticmethod
+    def ccl():
+        df3 = pd.read_csv('ccl.csv')
+        df3.fillna(method='ffill', inplace=True)
+        df3['fecha'] = pd.to_datetime(df3['fecha'],  format='%Y-%m-%d')
+        df3.rename(columns={'ultimo': 'ccl'}, inplace=True)
+        df3 = df3[['fecha','ccl']]
+        return df3
 
-def ccl():
-    df3 = pd.read_csv('ccl.csv')
-    df3.fillna(method='ffill', inplace=True)
-    df3['fecha'] = pd.to_datetime(df3['fecha'],  format='%Y-%m-%d')
-    df3.rename(columns={'ultimo': 'ccl'}, inplace=True)
-    df3 = df3[['fecha','ccl']]
-    return df3
+    @staticmethod
+    def mayorista():
+        df = pd.read_csv('dol-mayor.csv')
+        df['fecha'] = pd.to_datetime(df['fecha'],  format='%Y-%m-%d')
+        df.rename(columns={'ultimo': 'mayorista'}, inplace=True)
+        return df[['mayorista', 'fecha']]
 
-def mayorista():
-    df = pd.read_csv('dol-mayor.csv')
-    df['fecha'] = pd.to_datetime(df['fecha'],  format='%Y-%m-%d')
-     
+    @staticmethod
+    def riesgo_pais():
+        df2 = pd.read_csv('riesgo_pais.csv')
+        df2.fillna(method='ffill', inplace=True)
+        df2['fecha'] = pd.to_datetime(df2['fecha'],  format='%Y-%m-%d')
+        df2.rename(columns={'ultimo': 'riesgo'}, inplace=True)
+        df2 = df2[['fecha','riesgo']]
+        return df2
 
-    df.rename(columns={'ultimo': 'mayorista'}, inplace=True)
+    @staticmethod
+    def leer_bonos():
+        bonos_dict = {}
+        bonos = ['al30', 'al30d', 'gd30', 'gd30d']
+        for bono in bonos:
+            print(bono)
+            df1 = pd.read_csv(f'{bono}.csv')
+            df1['fecha'] = pd.to_datetime(df1['fecha'],  format='%Y-%m-%d')
+            df1.rename(columns={'cierre': bono, 'volumen': f'vol_{bono}'}, inplace=True)
+            df1[bono].replace(to_replace=0, method='ffill', inplace=True)
+            df1.fillna(method='ffill', inplace=True)
+            df1 = df1[['fecha',bono, f'vol_{bono}']]
+            bonos_dict[bono] = df1
 
+        return bonos_dict
 
-    return df[['mayorista', 'fecha']]
+    """
+    13 Week Treasury Bill (^IRX)
+    Treasury Yield 10 Years (^TNX)
+    Treasury Yield 30 Years (^TYX)
+    Treasury Yield 5 Years (^FVX)
+    """
+    @staticmethod
+    def referencias(tickers=['EEM', 'GGAL', 'YPF', '^TNX', '^TYX', '^FVX', '^IRX']):
+        t = yf.download(tickers,  '2020-01-02')['Adj Close']
+        t['fecha'] = t.index
+        usd_bonds = ['^TNX', '^TYX', '^FVX']
+        t[usd_bonds] = t[usd_bonds] / 100
+        return t
 
-def riesgo_pais():
-    df2 = pd.read_csv('riesgo_pais.csv')
-    df2.fillna(method='ffill', inplace=True)
-    df2['fecha'] = pd.to_datetime(df2['fecha'],  format='%Y-%m-%d')
-    df2.rename(columns={'ultimo': 'riesgo'}, inplace=True)
-    df2 = df2[['fecha','riesgo']]
-    return df2
+    @staticmethod
+    def create_df_wrk():
+        dolar_may = LeerCSVS.mayorista()
+        refs = LeerCSVS.referencias()
+        bonos_dict = LeerCSVS.leer_bonos()
+        df_riesgo_pais = LeerCSVS.riesgo_pais()
+        df_ccl = LeerCSVS.ccl()
 
-def leer_bonos():
-    bonos_dict = {}
-    bonos = ['al30', 'al30d', 'gd30', 'gd30d']
-    for bono in bonos:
-        print(bono)
-        df1 = pd.read_csv(f'{bono}.csv')
-        df1['fecha'] = pd.to_datetime(df1['fecha'],  format='%Y-%m-%d')
+        df_merge = bonos_dict['al30'].merge(df_riesgo_pais, on='fecha')
+        df_merge = df_merge.merge(df_ccl, on='fecha')
+        df_merge = df_merge.merge(bonos_dict['gd30d'], on='fecha')
+        df_merge = df_merge.merge(bonos_dict['al30d'], on='fecha')
+        df_merge = df_merge.merge(bonos_dict['gd30'], on='fecha')
+        df_merge = df_merge.merge(refs, on='fecha')
+        df_merge = df_merge.merge(dolar_may, on='fecha')
 
-        df1.rename(columns={'cierre': bono, 'volumen': f'vol_{bono}'}, inplace=True)
-        df1[bono].replace(to_replace=0, method='ffill', inplace=True)
-        df1.fillna(method='ffill', inplace=True)
+        df_merge['al30usd'] = df_merge.al30 / df_merge.ccl
+        df_merge['gd30usd'] = df_merge.gd30 / df_merge.ccl
 
-        df1 = df1[['fecha',bono, f'vol_{bono}']]
-        bonos_dict[bono] = df1
-
-    return bonos_dict
-
-"""
-13 Week Treasury Bill (^IRX)
-Treasury Yield 10 Years (^TNX)
-Treasury Yield 30 Years (^TYX)
-Treasury Yield 5 Years (^FVX)
-"""
-def referencias(tickers=['EEM', 'GGAL', 'YPF', '^TNX', '^TYX', '^FVX', '^IRX']):
-    t = yf.download(tickers,  '2020-01-02')['Adj Close']
-    t['fecha'] = t.index
-    usd_bonds = ['^TNX', '^TYX', '^FVX']
-    t[usd_bonds] = t[usd_bonds] / 100
-    return t
-
-def create_df_wrk():
-    dolar_may = mayorista()
-    refs = referencias()
-    bonos_dict = leer_bonos()
-    df_riesgo_pais = riesgo_pais()
-    df_ccl = ccl()
-
-    df_merge = bonos_dict['al30'].merge(df_riesgo_pais, on='fecha')
-    df_merge = df_merge.merge(df_ccl, on='fecha')
-    df_merge = df_merge.merge(bonos_dict['gd30d'], on='fecha')
-    df_merge = df_merge.merge(bonos_dict['al30d'], on='fecha')
-    df_merge = df_merge.merge(bonos_dict['gd30'], on='fecha')
-    df_merge = df_merge.merge(refs, on='fecha')
-    df_merge = df_merge.merge(dolar_may, on='fecha')
-
-    df_merge['al30usd'] = df_merge.al30 / df_merge.ccl
-    df_merge['gd30usd'] = df_merge.gd30 / df_merge.ccl
-
-    return df_merge
+        return df_merge
 
 def valor_bono_disc(pair_pagos, tasa, pagos_p_a=2):
     valor = 0
@@ -152,9 +151,9 @@ def curva_v_r(bono, fecha):
  
 if __name__ == '__main__':
    
-    # df_merge = create_df_wrk()
-    # with open('df_wrk.pkl', 'wb') as f:
-    #   pickle.dump(df_merge, f, protocol=pickle.HIGHEST_PROTOCOL )
+    df_merge = LeerCSVS.create_df_wrk()
+    with open('df_wrk.pkl', 'wb') as f:
+      pickle.dump(df_merge, f, protocol=pickle.HIGHEST_PROTOCOL )
 
     with open('df_wrk.pkl', 'rb') as f:
         df_merge = pickle.load(f)
