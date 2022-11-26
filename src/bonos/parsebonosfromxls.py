9f37c27a578aa4378115d18ef98607a92f4d8418
lmpizarro <lmpizarro@gmail.com>
2022-11-26 19:43:24 -0300

    added today

--- src/bonos/parsebonosfromxls.py
+++ src/bonos/parsebonosfromxls.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 url = 'https://iol.invertironline.com/mercado/cotizaciones/argentina/bonos/todos'
 
@@ -10,13 +11,10 @@ BONO['Ticker']=BONO['Símbolo']
 BONO['Precio']=pd.to_numeric(BONO['ÚltimoOperado'])
 BONO['Vol']=pd.to_numeric(BONO['MontoOperado'])
 precios_BONO = BONO[['Ticker', 'Precio', 'Vol']]
-precios_BONO.set_index('Ticker',inplace=True)
-
-print(precios_BONO.loc['AL30C']['Precio'])
-print(Data_BONO)
-print(precios_BONO)
+precios_BONO.set_index('Ticker', inplace=True)
 
 Data_BONO = pd.read_excel('cashflows_BONO.xls', sheet_name='DATA')
+
 usdars = 323
 try:
     usdars = float(input(f'input usdars ({usdars}): '))
@@ -24,6 +22,7 @@ except:
     pass
 cashflows={}
 for ticker in Data_BONO.Ticker:
+    today = datetime.now()
     iD = ticker
     multiplier = 1 / usdars
     if ticker[-1] in ['C', 'D']:
@@ -37,6 +36,7 @@ for ticker in Data_BONO.Ticker:
     CF.Renta = pd.to_numeric(CF.Renta)
     CF['Amortizacion'] = pd.to_numeric(CF['Amortización'])
     CF.drop(columns=['Amortización'], inplace=True)
+    CF = CF[CF['Fecha de pago'] >= today]
     cashflows[iD] = CF
 
     total_cash_flow = cashflows[iD].Renta.sum() +  cashflows[iD].Amortizacion.sum()
@@ -44,6 +44,7 @@ for ticker in Data_BONO.Ticker:
     yild =  total_cash_flow / precio
 
     time_to_maturity = (CF.iloc[-1]['Fecha de pago'] - CF.iloc[0]['Fecha de pago']).days / 365
+    time_to_maturity = (CF.iloc[-1]['Fecha de pago'] - today).days  / 365
 
     print(f'{iD} ', round(precio, 2), round(total_cash_flow,2), round(100 *  yild / time_to_maturity, 2))
 
