1c6e68f5c1c835784d673f796924930ebd5a0744
lmpizarro <lmpizarro@gmail.com>
2023-01-21 11:14:47 -0300

    a scrap ratio

--- src/scraper/scrap_bono_rava.py
+++ src/scraper/scrap_bono_rava.py
@@ -1,22 +1,35 @@
 from scrapers import scrap_bonos_rava
 import pandas as pd
 
+def test():
+    res = scrap_bonos_rava('gd41')
 
-res = scrap_bonos_rava('gd41')
+    coti_hist = pd.DataFrame(res['coti_hist'])
 
-coti_hist = pd.DataFrame(res['coti_hist'])
+    print(coti_hist.head())
 
-print(coti_hist.head())
+    flujo = pd.DataFrame(res['flujofondos']['flujofondos'])
+    dolar = (res['flujofondos']['dolar'])
+    tir = (res['flujofondos']['tir'])
+    duration = (res['flujofondos']['duration'])
 
-flujo = pd.DataFrame(res['flujofondos']['flujofondos'])
-dolar = (res['flujofondos']['dolar'])
-tir = (res['flujofondos']['tir'])
-duration = (res['flujofondos']['duration'])
+    print(flujo.head())
+    print(res.keys())
 
-print(flujo.head())
-print(res.keys())
+    print(res['cotizaciones'][0])
 
-print(res['cotizaciones'][0])
+    print(res['cuad_tecnico'])
 
-print(res['cuad_tecnico'])
+def coti_hist(res):
+    return pd.DataFrame(res['coti_hist'])
 
+def ratio(year: str = 29):
+    hist_gd = coti_hist(scrap_bonos_rava(f'gd{year}'))
+    hist_al = coti_hist(scrap_bonos_rava(f'al{year}'))
+
+    cierre_gd = hist_gd[['fecha', 'timestamp', 'cierre', 'usd_cierre']]
+    cierre_al = hist_al[['fecha', 'timestamp', 'cierre', 'usd_cierre']]
+    print(cierre_gd)
+    print(cierre_al)
+
+ratio()
