1914df2ad6e3e34d8eab9313f3475efdd12c170f
lmpizarro <lmpizarro@gmail.com>
2023-03-21 00:30:53 -0300

    BullCallSpread

--- src/options/pay_off_pl.py
+++ src/options/pay_off_pl.py
@@ -87,13 +87,36 @@ class ShortStrangle(LongStrangle):
         return - super().pay_off()
 
 class BullCallSpread(Option):
-    pass
+    def __init__(self, K1: float, P1: float, K2: float, P2: float) -> None:
+
+        if K2 <= K1 or P2 <= P1:
+            raise OptionError
+
+        self.K1 = K1
+        self.K2 = K2
+        self.P1 = P1
+        self.P2 = P2
+
+
+        self.prices = np.linspace(0, 1.5*self.K2)
+
+    def pay_off(self):
+
+        po_long_c = np.where(self.prices> self.K1, self.prices - self.K1, 0)
+
+        po_short_c = np.where(self.prices > self.K2, (self.K2- self.prices), 0)
+
+        po = (self.P2 +  self.P1)*(po_long_c + po_short_c) / (self.K2 - self.K1) - self.P1
+
+        return po
+
 
 
 lc = LongCall(100, 5)
 lp = LongPut(50, 5)
 # ls = ShortStraddle(100, 5)
 ls = ShortStrangle(50, 100, 5)
+ls = BullCallSpread(50, 2, 100, 5)
 # plt.plot(lc.prices, lc.pay_off())
 # plt.plot(lp.prices, lp.pay_off())
 # plt.plot(plplon)
@@ -101,9 +124,7 @@ ls = ShortStrangle(50, 100, 5)
 # plt.plot(lc.prices, lc.pay_off())
 # plt.plot(lp.prices, lp.pay_off())
 plt.plot(ls.prices, ls.pay_off())
-po = ls.pay_off()
-cs = make_interp_spline([ls.prices[0], ls.prices[50], ls.prices[75], ls.prices[99]], [po[0], po[50], po[75], po[99]])
-plt.plot(ls.prices, cs(ls.prices))
+# po = ls.pay_off()
+# cs = make_interp_spline([ls.prices[0], ls.prices[50], ls.prices[75], ls.prices[99]], [po[0], po[50], po[75], po[99]])
+# plt.plot(ls.prices, cs(ls.prices))
 plt.show()
-
-print(po.shape)
