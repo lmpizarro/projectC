09d570c16675b2459d9084f6915e8f18505ddd20
lmpizarro <lmpizarro@gmail.com>
2023-04-12 02:00:02 -0300

    add indicator

--- src/options/frac_diff.py
+++ src/options/frac_diff.py
@@ -3,7 +3,8 @@ import numpy as np
 from tsfracdiff import FractionalDifferentiator
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+import talib as ta
+
 import pandas as pd
 
 
@@ -12,37 +13,37 @@ tickers = ["SPY", "AAPL"]
 fracDiff = FractionalDifferentiator()
 
 dwldr = Downloader(start='2013-01-01', stocks=tickers)
-
 dwldr.download()
 dwldr.calc_log_return()
 
 print(dwldr.yf_data_close['SPY'].tail())
-print()
-print()
 print(dwldr.log_returns['SPY'].tail())
-ticker = 'SPY'
+
+ticker = 'AAPL'
 df_calc = pd.DataFrame()
 df_calc['close'] = dwldr.yf_data_close[ticker].copy()[1:]
 df_calc['return'] = np.array(dwldr.log_returns[ticker].copy())
 df_calc['prev_return'] = df_calc['return'].shift(1)
 df_calc['prev_close'] = df_calc['close'].shift(1)
-df_calc['ewm_return'] = df_calc['prev_return'].ewm(alpha=.7).mean()
-df_calc['ewm_close'] = df_calc['prev_close'].ewm(alpha=.7).mean()
+df_calc['ewm_return'] = df_calc['prev_return'].ewm(alpha=.5).mean()
+df_calc['ewm_close'] = df_calc['prev_close'].ewm(alpha=.5).mean()
+df_calc['rsi_close'] = ta.RSI(df_calc['prev_close'], timeperiod=14) / ta.RSI(df_calc['prev_close'], timeperiod=14).mean()
 df_calc['frac'] = fracDiff.FitTransform(df_calc['prev_close'], parallel=True)
+df_calc['MOM'] =  ta.MOM(df_calc['prev_close'], timeperiod=5)
 df_calc.dropna(inplace=True)
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
-features = ['prev_return', 'prev_close', 'ewm_return', 'ewm_close', 'frac']
+features = ['prev_return', 'prev_close', 'ewm_return', 'ewm_close', 'frac', 'rsi_close', 'MOM']
 df_train = df_calc[:1600]
 df_test = df_calc[1600:]
 
 pipe = Pipeline([('scaler', StandardScaler()), ('reg', LinearRegression())])
 
-print(df_calc.tail())
+print(df_calc.tail(8))
 print(df_calc.shape)
 pipe.fit(df_train[features], df_train['close'])
 sco = pipe.score(df_test[features], df_test['close'])
 print(sco)
 print(pipe.get_params())
-print(pipe.predict(df_test[features])[-7:])
+print(pipe.predict(df_test[features])[-8:])
