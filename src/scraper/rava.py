b89bc7ab108eb9d15784497c7c95891ffcb9c743
lmpizarro <lmpizarro@gmail.com>
2023-04-24 00:17:42 -0300

    reservas

--- src/scraper/rava.py
+++ src/scraper/rava.py
@@ -111,7 +111,9 @@ def variables_bcra(tipo='cer', desde='2016-04-20'):
         'mayorista': {"Serie": "272", "Detalle": "Tipo de Cambio Mayorista ($ por US$) Comunicación A 3500 - Referencia"},
         'TEAPF': {"Serie": "7939", "Detalle": "Tasa mínima para plazos fijos de personas humanas hasta $10 millones (en  e.a. para depósitos a 30 días)"},
         'inflacion': {"Serie": "7931", "Detalle": "Inflación mensual (variación en )"},
-        'inflacionIA': {"Serie": "7932", "Detalle": "Inflación interanual (variación en i.a.)"}
+        'inflacionIA': {"Serie": "7932", "Detalle": "Inflación interanual (variación en i.a.)"},
+        'reservas': {"Serie": "246", "Detalle": "Reservas Internacionales del BCRA (en millones de dólares - cifras provisorias sujetas a cambio de valuación)"},
+
     }
     today = datetime.now().date()
     month = f'0{today.month}' if today.month >= 1 and today.month <= 9 else f'{today.month}'
@@ -131,8 +133,10 @@ def variables_bcra(tipo='cer', desde='2016-04-20'):
     r_text = resp.text
 
     df_cer = pd.read_html(r_text, thousands='.')[0]
+    print(df_cer.tail())
 
-    df_cer = df_cer.apply(lambda x: x.str.replace(',','.'))
+    if tipo != 'reservas':
+        df_cer = df_cer.apply(lambda x: x.str.replace(',','.'))
     df_cer['Fecha'] = pd.to_datetime(df_cer['Fecha'],format= '%d/%m/%Y' ).dt.date
     df_cer[['Valor']] = df_cer[['Valor']].astype('float64')
     df_cer.set_index('Fecha', inplace=True)
@@ -143,7 +147,7 @@ import matplotlib.pyplot as plt
 # df_cer = variables_bcra('cer')
 # print(df_cer.head())
 # print(df_cer.tail())
-df_cer = variables_bcra('inflacion', desde='2015-12-17')
+df_cer = variables_bcra('reservas', desde='2001-12-17')
 
 print(df_cer.tail())
 plt.plot(df_cer)
