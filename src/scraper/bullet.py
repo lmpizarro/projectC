858c83514460afb676f6e7cc08a937cfb2bf8f6f
lmpizarro <lmpizarro@gmail.com>
2023-04-28 02:03:39 -0300

    test

--- src/scraper/bullet.py
+++ src/scraper/bullet.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-years = 3
-compound = 2
+years = 1
+compound = 4
 rate = .1
 
 n_pays = years * compound
@@ -12,14 +12,14 @@ amortizations = np.zeros(n_pays)
 amortizations[n_pays - 1] = 1
 description = np.asarray([times, coupons, amortizations])
 
-r = 0.1
+r = 0.09
 rc = compound * np.log(1 + r /compound)
 
 
 time = np.linspace(1/365, years, 365*years, endpoint=False )
 npvs = np.zeros(time.shape)
-for i,t in enumerate(time):
-    remaining = description[0] - t
+for i in range(time.shape[0]):
+    remaining = description[0] - time[i]
     mask = np.where( remaining <= 0, 0, 1)
 
     description = mask * description
