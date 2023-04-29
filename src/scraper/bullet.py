a2c51f4780be6d4f5c2f9e7fa579fcb29c61c5e3
lmpizarro <lmpizarro@gmail.com>
2023-04-28 21:37:32 -0300

    rates

--- src/scraper/bullet.py
+++ src/scraper/bullet.py
@@ -2,21 +2,37 @@ import numpy as np
 
 years = 1
 compound = 4
-rate = .1
+year_rate = .1
 
 n_pays = years * compound
-coupon = rate / compound
+coupon = year_rate / compound
 times = np.linspace(1/compound,years,n_pays)
 coupons = np.ones(n_pays)*coupon
 amortizations = np.zeros(n_pays)
 amortizations[n_pays - 1] = 1
 description = np.asarray([times, coupons, amortizations])
 
-r = 0.09
-rc = compound * np.log(1 + r /compound)
+rate = .1
+
+def constant_rate(time, rate: float = .1):
+
+    rates = np.ones(time.shape[0])*rate
+    rcs = compound * np.log(1 +  rates / compound)
+    return rcs
+
+def exp_rate(time, rate: float=.1, speed: float = -4):
+    rates = np.ones(time.shape[0])*rate
+    rates = np.exp(speed * time) * rates
+    if speed >= 0:
+        rates = (1 - np.exp(-speed * time)) * rates
+
+    rcs = compound * np.log(1 +  rates / compound)
+    return rcs
+
 
 
 time = np.linspace(1/365, years, 365*years, endpoint=False )
+rcs = exp_rate(time=time)
 npvs = np.zeros(time.shape)
 for i in range(time.shape[0]):
     remaining = description[0] - time[i]
@@ -24,11 +40,13 @@ for i in range(time.shape[0]):
 
     description = mask * description
     pays = description[1] + description[2]
-    npv = np.exp(-remaining*rc) * pays
+    npv = np.exp(-remaining*rcs[i]) * pays
     npvs[i] = npv.sum()
 
 import matplotlib.pyplot as plt
 plt.plot(npvs)
+plt.show()
+plt.plot(rcs)
 plt.show()
 
 
