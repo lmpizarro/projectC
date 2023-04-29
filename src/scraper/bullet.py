b237df1c9d4f3ed10d0ec0b8c2951584d11220b1
lmpizarro <lmpizarro@gmail.com>
2023-04-29 13:53:23 -0300

    nov method

--- src/scraper/bullet.py
+++ src/scraper/bullet.py
@@ -5,17 +5,22 @@ DAYS_IN_A_YEAR = 365
 
 class RateGenerators:
 
-    def __init__(self, years: np.ndarray) -> None:
+    def __init__(self, years: np.ndarray, compound:int = 2) -> None:
         self._time = np.linspace(1/DAYS_IN_A_YEAR, years, DAYS_IN_A_YEAR*years, endpoint=False )
+        self._compound = compound
 
     @property
     def time(self) -> np.ndarray:
         return self._time
 
+    @property
+    def compound(self) -> int:
+        return self._compound
+
     def constant_rate(self, rate: float = .1) -> np.ndarray:
 
         rates = np.ones(self.time.shape[0])*rate
-        return RateGenerators.discrete_to_continuous(rates=rates)
+        return RateGenerators.discrete_to_continuous(rates=rates, compound=self.compound)
 
     def exp_rate(self, rate: float=.1, speed: float = -4) -> np.ndarray:
         rates = np.ones(self.time.shape[0])*rate
@@ -23,19 +28,23 @@ class RateGenerators:
         if speed >= 0:
             rates = (1 - np.exp(-speed * self.time)) * rates
 
-        return RateGenerators.discrete_to_continuous(rates=rates)
+        return RateGenerators.discrete_to_continuous(rates=rates, compound=self.compound)
 
     @staticmethod
-    def discrete_to_continuous(rates: np.ndarray) -> np.ndarray:
+    def discrete_to_continuous(rates: np.ndarray, compound: int) -> np.ndarray:
         return compound * np.log(1 +  rates / compound)
+
+    @staticmethod
+    def continuous_to_discrete(rates:np.ndarray, compound: int) -> np.ndarray:
+        return compound * (np.exp(rates / compound) - 1)
 
     def sin_rate(self, rate: float=.1, amplitude: float = .02) -> np.ndarray:
         rates = np.sin(2*np.pi * self.time / self.time[-1])*amplitude + rate
 
-        return RateGenerators.discrete_to_continuous(rates=rates)
+        return RateGenerators.discrete_to_continuous(rates=rates, compound=self.compound)
 
 
-years = 1
+years = 3
 compound = 4
 year_rate = .1
 
@@ -45,27 +54,32 @@ times = np.linspace(1/compound,years,n_pays)
 coupons = np.ones(n_pays)*coupon
 amortizations = np.zeros(n_pays)
 amortizations[n_pays - 1] = 1
-description = np.asarray([times, coupons, amortizations])
+description = np.asarray([times, coupons, amortizations, coupons + amortizations])
 
 rate = .1
 
-tg = RateGenerators(years=years)
+tg = RateGenerators(years=years, compound=compound)
 
 rcs = tg.sin_rate()
 rcs_ = tg.exp_rate()
+rcs = tg.constant_rate()
 npvs = np.zeros(tg.time.shape)
+
+def npv(description:np.ndarray, time: np.ndarray, indx: int) -> float:
+    remaining = description[0] - time[indx]
+    description = np.where( remaining <= 0, 0, 1) * description
+    npv = np.exp(-remaining*rcs[i]) * description[3]
+    return npv.sum()
+
 for i in range(tg.time.shape[0]):
     remaining = description[0] - tg.time[i]
-    mask = np.where( remaining <= 0, 0, 1)
-
-    description = mask * description
-    pays = description[1] + description[2]
-    npv = np.exp(-remaining*rcs[i]) * pays
+    description = np.where( remaining <= 0, 0, 1) * description
+    npv = np.exp(-remaining*rcs[i]) * description[3]
     npvs[i] = npv.sum()
 
 plt.plot(npvs)
 plt.show()
-plt.plot(rcs)
+plt.plot(RateGenerators.continuous_to_discrete(rcs, compound=compound))
 plt.show()
 
 
