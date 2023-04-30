41a0791478e11930637e389dcd54e1cc3e6ae2eb
lmpizarro <lmpizarro@gmail.com>
2023-04-30 18:38:11 -0300

    sep things

---
+++ src/scraper/us_yields.py
@@ -0,0 +1,94 @@
+import requests
+from config import urls, url_treasury_by_year
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+class USBonds:
+    def __init__(self, year: int = 2023) -> None:
+        self.year = year
+        self.yield_curve = self.treasury_yield_curve()
+
+    def treasury_yield_curve(self):
+        string_terms = [
+            "1 Mo",
+            "2 Mo",
+            "3 Mo",
+            "4 Mo",
+            "6 Mo",
+            "1 Yr",
+            "2 Yr",
+            "3 Yr",
+            "5 Yr",
+            "7 Yr",
+            "10 Yr",
+            "20 Yr",
+            "30 Yr",
+        ]
+
+        resp = requests.get(
+            url_treasury_by_year(year=self.year), headers={"User-Agent": "Mozilla/5.0"}
+        )
+        treas_df = pd.read_html(resp.text)
+
+        filter_keys = ["Date"]
+        filter_keys.extend(string_terms)
+        treas_df = treas_df[0][filter_keys]
+
+        def spliter(r: str):
+            s = r.split(" ")
+            if s[1] == "Mo":
+                d = 30
+            else:
+                d = 365
+            k = int(s[0])
+
+            return d * k
+
+        treas_df["Date"] = pd.to_datetime(treas_df["Date"], format="%m/%d/%Y").dt.date
+        string_term_to_days_term = {r: spliter(r) for r in string_terms}
+        days_term = string_term_to_days_term.values()
+        treas_df.rename(columns=string_term_to_days_term, inplace=True)
+        treas_df['mean'] = treas_df[days_term].mean(axis=1)
+        treas_df.set_index('Date', inplace=True)
+
+        return treas_df
+
+    def today_mean(self):
+        today_year = datetime.now().year
+        if self.year != today_year:
+            self.year = today_year
+            self.yield_curve = self.treasury_yield_curve()
+        return self.yield_curve['mean'].iloc[-1]
+
+    def last_curve_points(self):
+
+        terms = np.array(list(self.yield_curve.keys())[0:-1])
+        rates = np.array(list(self.yield_curve.tail().iloc[-1])[0:-1])
+
+        return terms, rates
+
+
+if __name__ == '__main__':
+
+    usbond = USBonds()
+    df_treas = usbond.yield_curve
+    print(df_treas.tail())
+    print(usbond.today_mean())
+    terms, rates = usbond.last_curve_points()
+
+    daily_rates = rates / 365
+    term_rates = daily_rates * terms
+
+    print(terms)
+    print(rates)
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(df_treas['mean'])
+    plt.show()
+
+    plt.plot(terms, rates)
+    plt.show()
+
+
---
+++ src/scraper/variables_bcra.py
@@ -0,0 +1,80 @@
+import requests
+import pandas as pd
+from datetime import datetime
+
+
+def variables_bcra(tipo="cer", desde="2016-04-20"):
+    url = "https://www.bcra.gob.ar/PublicacionesEstadisticas/Principales_variables_datos.asp"
+    settings = {
+        "cer": {"Serie": "3540", "Detalle": "CER (Base 2.2.2002=1)"},
+        "badlar": {
+            "Serie": "7935",
+            "Detalle": "BADLAR en pesos de bancos privados (en  e.a.)",
+        },
+        "TEAPolMon": {
+            "Serie": "7936",
+            "Detalle": "Tasa de Política Monetaria (en  e.a.)",
+        },
+        "mayorista": {
+            "Serie": "272",
+            "Detalle": "Tipo de Cambio Mayorista ($ por US$) Comunicación A 3500 - Referencia",
+        },
+        "TEAPF": {
+            "Serie": "7939",
+            "Detalle": "Tasa mínima para plazos fijos de personas humanas hasta $10 millones (en  e.a. para depósitos a 30 días)",
+        },
+        "inflacion": {"Serie": "7931", "Detalle": "Inflación mensual (variación en )"},
+        "inflacionIA": {
+            "Serie": "7932",
+            "Detalle": "Inflación interanual (variación en i.a.)",
+        },
+        "reservas": {
+            "Serie": "246",
+            "Detalle": "Reservas Internacionales del BCRA (en millones de dólares - cifras provisorias sujetas a cambio de valuación)",
+        },
+    }
+    today = datetime.now().date()
+    month = (
+        f"0{today.month}" if today.month >= 1 and today.month <= 9 else f"{today.month}"
+    )
+    hasta = f"{today.year}-{month}-{today.day}"
+    data = {
+        "primeravez": "1",
+        "fecha_desde": desde,
+        "fecha_hasta": hasta,
+        "serie": settings[tipo]["Serie"],
+        "series1": "0",
+        "series2": "0",
+        "series3": "0",
+        "series4": "0",
+        "detalle": settings[tipo]["Detalle"],
+    }
+    resp = requests.post(url=url, data=data, headers={"User-Agent": "Mozilla/5.0"})
+
+    r_text = resp.text
+
+    df_cer = pd.read_html(r_text, thousands=".")[0]
+    print(df_cer.tail())
+
+    if tipo != "reservas":
+        df_cer = df_cer.apply(lambda x: x.str.replace(",", "."))
+    df_cer["Fecha"] = pd.to_datetime(df_cer["Fecha"], format="%d/%m/%Y").dt.date
+    df_cer[["Valor"]] = df_cer[["Valor"]].astype("float64")
+    df_cer.set_index("Fecha", inplace=True)
+    df_cer.rename(columns={"Valor": tipo}, inplace=True)
+    return df_cer
+
+def test_ccl():
+    import matplotlib.pyplot as plt
+
+    # df_cer = variables_bcra('cer')
+    # print(df_cer.head())
+    # print(df_cer.tail())
+    df_cer = variables_bcra("reservas", desde="2001-12-17")
+
+    print(df_cer.tail())
+    plt.plot(df_cer)
+    plt.show()
+
+if __name__ == '__main__':
+    variables_bcra()
